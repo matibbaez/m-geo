fb48a380a87d9445b84c6ca2fa58a6dab9a5f6ba
matibbaez <matiascaneda3@gmail.com>
2026-06-02 12:04:32 -0300

    Fix dependencias de Vercel para Astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel/serverless'; // 1. Importamos el adaptador
+import vercel from '@astrojs/vercel'; // Nota: antes era /serverless, ahora suele ser solo /vercel
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'server', // 2. Definimos el modo server
-  adapter: vercel(), // 3. Activamos el adaptador
+  output: 'server',
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   }
