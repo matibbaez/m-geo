f2f12824f1fdfe7efbeb512166e7fe25ad186e8d
matibbaez <matiascaneda3@gmail.com>
2026-06-08 11:00:14 -0300

    feat: integra sitemap y verificacion de google search console

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel'; // Nota: antes era /serverless, ahora suele ser solo /vercel
+import vercel from '@astrojs/vercel'; 
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://www.m-geo.ar',
   output: 'server',
   adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [sitemap()]
 });
